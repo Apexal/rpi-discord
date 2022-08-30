ff98fb434f2ac9c4bb41baad7c7d508788f67cf9
Frank Matranga <thefrankmatranga@gmail.com>
2022-08-30 11:10:33 -0400

    Define roles

--- src/roles.ts
+++ src/roles.ts
@@ -1,10 +1,16 @@
 import { ButtonStyle, ComponentEmojiResolvable } from "discord.js";
 
 export type UserRole = {
+  /** User-facing label */
   label: string;
+  /** Internal unique ID */
   customId: string;
+  /** User-facing button style */
   buttonStyle: ButtonStyle;
-  buttonEmoji: ComponentEmojiResolvable;
+  /** User-facing emoji for button and role shorthand */
+  emoji: ComponentEmojiResolvable;
+  /** Discord role ID */
+  roleId?: string;
 };
 
 export const roles: UserRole[] = [
@@ -12,24 +18,24 @@ export const roles: UserRole[] = [
     label: "Current RPI Student",
     customId: "current-rpi-student",
     buttonStyle: ButtonStyle.Primary,
-    buttonEmoji: "👩‍🎓",
+    emoji: "👩‍🎓",
   },
   {
     label: "Current RPI Faculty",
     customId: "current-rpi-faculty",
     buttonStyle: ButtonStyle.Secondary,
-    buttonEmoji: "🧑‍🏫",
+    emoji: "🧑‍🏫",
   },
   {
     label: "RPI Alumn",
     customId: "current-rpi-alumn",
     buttonStyle: ButtonStyle.Secondary,
-    buttonEmoji: "🧑‍🎓",
+    emoji: "🧑‍🎓",
   },
   {
     label: "Guest",
     customId: "guest",
     buttonStyle: ButtonStyle.Danger,
-    buttonEmoji: "👋",
+    emoji: "👋",
   },
 ];
